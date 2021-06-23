d7a927003c874f7a52ecbeba2c362f8026c0fe3c
Alain Di Chiappari <alain.devcs@gmail.com>
2021-06-23 12:50:38 +0200

    input parsing, stubbing some TS stuff for now

---
+++ src/index.ts
@@ -0,0 +1,45 @@
+'use strict';
+
+import fs from 'fs';
+
+function solve(bitmap: any) {
+  return bitmap;
+}
+
+function printResult(result: number[][]) {
+  for (const row of result) {
+    console.log(row.join(' '));
+  }
+  console.log('\n');
+}
+
+function main() {
+  const data = fs.readFileSync(0, 'utf-8');
+  const lines = data.split('\n');
+  let currLine = 0;
+
+  const numTests: number = parseInt(lines[currLine].trim());
+  currLine++;
+
+  for (let i: number = 0; i < numTests; i++) {
+    const [n, m] = lines[currLine].trim().split(' ').map(x => parseInt(x));
+    currLine++;
+
+    let bitmap: number[][] = Array();
+
+    for (let r: number = 0; r < n; r++) {
+      const row: number[] = lines[currLine].trim().split('').map(x => parseInt(x));
+      currLine++;
+      if (row.length !== m) {
+        throw new Error('wrong amount of columns')
+      }
+      bitmap.push(row);
+    }
+    const result = solve(bitmap);
+    printResult(result);
+    // test cases divider empty row
+    currLine++;
+  }
+}
+
+main();
